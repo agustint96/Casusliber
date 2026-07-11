03eba4ba8835d496129344308673eff45c517076
Agus <92907673+agustint96@users.noreply.github.com>
2026-07-11 12:53:05 -0300

    Cuadro de sator

---
+++ niveles/nivel17.js
@@ -0,0 +1,10 @@
+// Nivel generado con el Creador de niveles de Casus Liber
+window.LEVELS[16] = [
+  "",
+  "                                S A T O R(*)",
+  "                                A R E P O",
+  "                                T E N E T",
+  "                                O P E R A",
+  "                                (^)R O T A S"
+];
+window.LEVEL_TITLES[16] = "Cuadro de sator";
